45c643667fa06d48823e7b2d476da4388cd7d85f
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-02-25 16:47:59 +0500

    (chore)Tests working again

--- resources/users/controller.js
+++ resources/users/controller.js
@@ -1,4 +1,4 @@
-const { remove, getUserRanking, topRated, updateProfile } = require('./model');
+const { remove, updateProfile } = require('./model');
 
 exports.deleteUser = async (req, res) => {
   try {
@@ -6,37 +6,6 @@ exports.deleteUser = async (req, res) => {
     res.status(200).json({ message: 'User successfully deleted' });
   } catch (error) {
     res.status(500).json({ message: error.message });
-  }
-};
-
-exports.getUserScore = async (req, res) => {
-  try {
-    const { subject } = req.decodedToken;
-    const [user, score] = await getUserRanking(subject);
-
-    const userWithScore = {
-      ...score,
-      ...user,
-    };
-    if (userWithScore.score === undefined) {
-      userWithScore.score = 0;
-    }
-    res.status(200).json({
-      message: `Successfully fetched User score`,
-      data: userWithScore,
-    });
-  } catch (error) {
-    res.status(500).json({ message: `Failed to fetch score.` });
-  }
-};
-
-exports.getLeaderboard = async (req, res) => {
-  try {
-    const leaderboard = await topRated();
-
-    res.status(200).json({ message: 'Fetched leaderboard', data: leaderboard });
-  } catch (error) {
-    res.status(500).json({ message: `Failed to fetch leaderboard.` });
   }
 };
 
